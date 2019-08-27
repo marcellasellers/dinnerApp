8ef36dd2bcfcfa6ed8adec60ec0993199257e23e
Marcella Sellers <mseller3@asu.edu>
2019-08-27 12:19:55 -0500

    minor tweaks and removals

--- src/app.js
+++ src/app.js
@@ -1,29 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import DinnerApp from './components/DinnerApp.js'
-import Search from './components/Search.js'
-import css from '../public/stylesheets/landing.css';
-
-class User {
-    constructor (name = 'Anon', location) {
-        this.name = name;
-        this.location = location;
-        this.favRestaurants = [];
-    };
-
-    addRestaurant (restaurant) {
-        this.favRestaurants.push(restaurant);
-    };
-
-    updateName (newName) {
-        this.name = newName;
-    };
-
-    updateLocation (newLocation) {
-        this.location = newLocation;
-    };
-};
-const me = new User('Marcie', 'Zd');
+import '../public/stylesheets/landing.css';
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(<DinnerApp user={me}/>, appRoot);
+ReactDOM.render(<DinnerApp/>, appRoot);
